b83e5ff535b7d73aa8381cec02f20f680f832d84
flaim <likeyt60@gmail.com>
2025-02-19 01:19:16 +0300

    update

--- src/main/java/net/flaim/skillsPlugin/SkillsPlugin.java
+++ src/main/java/net/flaim/skillsPlugin/SkillsPlugin.java
@@ -2,24 +2,17 @@ package net.flaim.skillsPlugin;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SkillsPlugin extends JavaPlugin {
+public final class SkillsPlugin extends JavaPlugin implements Listener {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    private final SkillManager skillManager = new SkillManager();
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
+        String playerName = event.getPlayer().getName();
+        skillManager.getOrCreatePlayerData(playerName);
     }
 }
